2bfce829b6c6a169f9ed3d4c475f129d3bd038a3
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-08-04 11:40:59 +0900

    코드 추가, 삭제 액티비티 추가

---
+++ main/java/com/example/mainactivity/CodeDeleteActivity.kt
@@ -0,0 +1,14 @@
+package com.example.mainactivity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CodeDeleteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.delete_activity)
+
+
+
+    }
+}
--- main/java/com/example/mainactivity/MainActivity.kt
+++ main/java/com/example/mainactivity/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mainactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         //상단 제목 표시줄 숨기기
         supportActionBar?.hide()
 
+
+        val createBtn = findViewById<Button>(R.id.create_code_btn)
+        createBtn.setOnClickListener{
+            val intent = Intent(this, CodeCreateActivity::class.java)
+            startActivity(intent)
+        }
+
+//        val deleteBtn = findViewById<Button>(R.id.delete_code_btn)
+//        deleteBtn.setOnClickListener{
+//            val intent = Intent(this, CodeDeleteActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
