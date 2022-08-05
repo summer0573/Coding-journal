067cce6538a77eb79580845d67f345fc8655e374
Jung Hyun Jin <83990983+summer0573@users.noreply.github.com>
2022-08-05 14:06:02 +0900

    뭐가 문제인가요

--- main/java/com/example/mainactivity/CodeCreateActivity.kt
+++ main/java/com/example/mainactivity/CodeCreateActivity.kt
@@ -3,22 +3,29 @@ package com.example.mainactivity
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class CodeCreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.create_activity)
 
-        val pref = getSharedPreferences("journals", Context.MODE_PRIVATE)
-        val journals = Journals.getJournalsFromPreference(pref)
+        //val pref = getSharedPreferences("journals", Context.MODE_PRIVATE)
+        //val journals = Journals.getJournalsFromPreference(pref)
 
         val editJournals = mutableListOf<Journals>()
         for(i in 0 until 20) {
             editJournals.add(Journals("", "", ""))
         }
-        for(j in journals) {
-            editJournals
-        }
+
+        val layoutManager = LinearLayoutManager(this)
+        val adapter = CodeCreateAdapter(editJournals)
+
+        val listView = findViewById<RecyclerView>(R.id.create_adapter)
+        listView.setHasFixedSize(false)
+        listView.layoutManager = layoutManager
+        listView.adapter = adapter
 
     }
 }
